5210aabb55fa8835a0bed48cf7994a4b47d220c8
Nick Gal <nickgal@me.com>
2015-10-22 19:34:29 -0700

    style change => double to single quotes

--- lib/emojipacks_twitch.rb
+++ lib/emojipacks_twitch.rb
@@ -9,12 +9,12 @@ class EmojipacksTwitch
     json_file = open('http://twitchemotes.com/api_cache/v2/images.json')
     images_hash = JSON.parse json_file.read
     output_hash = {
-      "title" => 'Twitch'
+      'title' => 'Twitch'
     }
-    output_hash["emojis"] = images_hash["images"].map do |image_id, details|
+    output_hash['emojis'] = images_hash['images'].map do |image_id, details|
       {
-        "name" => details["code"].downcase,
-        "src" => "https://static-cdn.jtvnw.net/emoticons/v1/#{image_id}/1.0"
+        'name' => details['code'].downcase,
+        'src' => "https://static-cdn.jtvnw.net/emoticons/v1/#{image_id}/1.0"
       }
     end
     File.open('twitch.yml', 'w') do |f|
